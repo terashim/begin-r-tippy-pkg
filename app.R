daa7ff4ed7fd916b9cac1ceabb282c24e71d00ad
Terashi, Masaki <terashi.masaki@gmail.com>
2018-12-09 13:37:44 +0900

    tippyパッケージでツールチップを追加

--- app.R
+++ app.R
@@ -1,24 +1,58 @@
 library(shiny)
+library(tippy)
 
 # アプリケーションのUIを定義
 ui <- fluidPage(
   
   # アプリケーションのタイトル
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Shinyアプリにtippyパッケージでツールチップを追加"),
   
   sidebarLayout(
     # サイドバー
     sidebarPanel(
-      # スライダー
-      sliderInput("bins", "ビンの数:", min = 1, max = 50, value = 30)
+      
+      # スライダー入力にツールチップを付けるための工夫 = ラッパーdiv要素で囲む
+      # tippyバージョン0.1.0ではsliderInputとselectInputに直接ツールチップを付けられない
+      # Cf. https://github.com/JohnCoene/tippy/issues/2
+      div(
+        id = "bins-wrapper",
+        # スライダー入力
+        sliderInput("bins", "ビンの数:", min = 1, max = 50, value = 30)
+      ),
+      # スライダー入力ウィジェットにツールチップを付ける
+      tippy_this("bins-wrapper", tooltip = "スライダー！!"),
+      
+      # テキスト入力
+      textInput("text", "テキストを入力"),
+      # テキスト入力ウィジェットにツールチップを付ける
+      tippy_this("text", tooltip = "テキスト！！"),
+      
+      
+      p(
+        "これは何？", 
+        # font-awesomeのアイコンにツールチップを付けるため、ラッパーspan要素で囲む
+        span(icon("info-circle"), id = "info"),
+        tippy_this(
+          elementId = "info",
+          tooltip = "ヘルプです！！",
+          placement = "right" # 右側にツールチップを出すオプション
+        )
+      )
     ),
     
     # メインパネル
     mainPanel(
-      plotOutput("distPlot")
+      # ラッパーdiv要素で囲み、idを付ける
+      div(
+        id = "plot",
+        plotOutput("distPlot")
+      ),
+      # ツールチップを付ける
+      tippy_this("plot", "プロットです！！！")
     )
   )
 )
+
 # サーバー側ロジックを定義
 server <- function(input, output) {
   
